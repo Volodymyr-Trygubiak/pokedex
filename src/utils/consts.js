6e7d03a4a155d8f79bf327b45e20c38dc2112244
Volodymyr Trygubiak <trigubiak@gmail.com>
2021-10-05 22:35:34 +0300

    added .env file and create consts

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -2,3 +2,15 @@
 export const LIST_ROUTE = '/'
 export const INFO_ROUTE = '/info/:id'
 export const ERROR_ROUTE = '/error'
+
+// Actions const
+
+export const LOAD_START = "LOAD_START"
+export const LOAD_SUCC = "LOAD_SUCC"
+export const LOAD_ERR = "LOAD_ERR"
+
+export const LOAD_DETAILS = "LOAD_DETAILS"
+
+// Urls
+
+export const BASE_URL = process.env.REACT_APP_BASE_URL
